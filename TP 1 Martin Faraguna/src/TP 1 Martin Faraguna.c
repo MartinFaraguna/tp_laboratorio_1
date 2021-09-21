595249d3d4e25583abc0ca66b6825585910bb4b6
MartinFaraguna <faraguna.martin@gmail.com>
2021-09-21 01:38:01 -0300

    segundo commit

--- TP 1 Martin Faraguna/src/TP 1 Martin Faraguna.c
+++ TP 1 Martin Faraguna/src/TP 1 Martin Faraguna.c
@@ -8,10 +8,44 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "Calculadora.h"
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+	setbuf(stdout, NULL);
+
+	int num1;
+	int num2;
+
+	int opcion;
+
+	printf("Bienvenido, elija una opcion \n");
+	do {
+		opcion = menu();
+
+
+		switch (opcion) {
+		case 1:
+			printf("Usted ha seleccionado 1 Ingrese el Primer Operando\n");
+			op1(&num1);
+			break;
+		case 2:
+			printf("Usted ha seleccionado 2	Ingrese el Segundo Operando \n");
+			op2(&num2);
+			break;
+		case 3:
+			printf("Usted ha seleccionado 3 Calcular Operaciones \n");
+			break;
+		case 4:
+			printf("Usted ha seleccionado 4 Mostrar Resultados \n");
+			break;
+		case 5:
+			printf("Gracias, vuelva prontos!!\n");
+			break;
+		default:
+			printf("\nOpcion incorrecta, intente nuevamente. \n");
+		}
+	} while (opcion != 5);
+
+	return 0;
 }
